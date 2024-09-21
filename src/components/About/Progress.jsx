a27d6420a4ce11cce7f44e6bff027368f0af8013
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2024-09-21 15:12:37 +0530

    Technical Skills added and some bug fixes given

--- src/components/About/Progress.jsx
+++ src/components/About/Progress.jsx
@@ -16,10 +16,14 @@ export default function CircularProgressDeterminate(props) {
   // }, []);
 
   return (
-    <Box sx={{ display: 'flex', gap: 3, alignItems: 'center', flexWrap: 'wrap' }}>
+    <Box sx={{ display: 'flex', gap: 2, alignItems: 'center', flexWrap: 'wrap' }}>
 
-        <img src={`/${props.image}`} />
-        <CircularProgress determinate value={props.prof} size="lg"/>
+          <img src={`/${props.image}`} style={{height: `${props.height}`, width: `${props.width}`}}/>
+
+        <CircularProgress determinate value={props.prof} size="md" variant="plain">
+          <div style={{color: "white", fontSize: "13px"}}>{props.prof}</div>
+        </CircularProgress>
+
 
     </Box>
   );
--- src/components/About/Skills.jsx
+++ src/components/About/Skills.jsx
@@ -9,16 +9,48 @@ function Skills() {
 
       <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: "70px"}}>
 
-        <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
+        <div style={{display: "flex", flexDirection: "column", gap: "20px", alignItems: "center"}}>
           <div style={{color: "white", fontSize: "25px", fontWeight: "50px"}}>Programming Languages</div>
 
-          <Progress image="cpp.png" prof={25} />
+          <Progress image="cpp.svg" prof={85} height="48px" width="48px" />
+          <Progress image="python.svg" prof={70} height="48px" width="48px"/>
+          <Progress image="sql.svg" prof={70} height="48px" width="48px"/>
 
         </div>
 
 
-        <div>
+        <div style={{display: "flex", flexDirection: "column", gap: "20px", alignItems: "center"}}>
           {/* web */}
+
+          <div style={{color: "white", fontSize: "25px", fontWeight: "50px"}}>Web Technology</div>
+
+          <div style={{display: "flex", flexDirection: "row", gap: "60px"}}>
+
+
+                {/* flex 1 */}
+                <div style={{display: "flex", flexDirection: "column", gap: "20px", alignItems: "center"}}> 
+                    <Progress image="html.svg" prof={85} height="48px" width="48px" />
+                    <Progress image="css.svg" prof={85} height="48px" width="48px" />
+                    <Progress image="js.svg" prof={75} height="48px" width="48px" />
+                    <Progress image="ts.svg" prof={65} height="48px" width="48px" />
+                    <Progress image="tail.svg" prof={60} height="48px" width="48px" />
+                    <Progress image="boot.svg" prof={70} height="48px" width="48px" />
+                </div>
+
+                <div style={{ width: "3px", backgroundColor: "white", height: "100%", opacity: "0.6"}}></div>
+
+                {/* flex 2 */}
+                <div style={{display: "flex", flexDirection: "column", gap: "20px", alignItems: "center"}}>
+                    <Progress image="mongo.svg" prof={75} height="48px" width="48px" />
+                    <Progress image="express.svg" prof={70} height="48px" width="48px" />
+                    <Progress image="react.svg" prof={80} height="48px" width="48px" />
+                    <Progress image="node.svg" prof={75} height="48px" width="48px" />
+                    <Progress image="solidity.svg" prof={75} height="48px" width="48px" />
+                    <Progress image="php.svg" prof={75} height="48px" width="48px" />
+                </div>
+          </div>
+
+
         </div>
 
       </div>
